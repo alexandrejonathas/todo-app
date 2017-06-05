97522b36692a853d64147fa256d2b946bc25e6b5
Jonathas Lima <alexandrejonathas@gmail.com>
2017-06-05 13:04:56 -0300

    Comitando o projento com a estrutura de frontend pronta

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
 
 server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.user(allowCors)
 
 const port = 3000
 server.listen(port, function(){
