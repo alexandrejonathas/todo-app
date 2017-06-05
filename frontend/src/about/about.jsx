de3383b4cb19f37d418726d44370ae17e5e247da
Jonathas Lima <alexandrejonathas@gmail.com>
2017-06-05 18:52:26 -0300

    Comitando o projeto com o bind do input funcionando

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós' />
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <h2>Missão e visão</h2>
+    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <h2>Imprensa</h2>
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 )
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+  <div>
+    <ul>
+      <li>Listagem das tarefas</li>        
+    </ul>
+  </div>
+)
